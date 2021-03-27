0ce846cc8f583bd35778a9eab4e075f054fa4f91
Lanndrich <lanndrich@gmail.com>
2021-03-27 21:55:20 +0000

    App fetches inspirational quotes and gives them on command.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const client = new Discord.Client();
+
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      return data[0]["q"] + "  - " + data[0]["a"];
+    });
+}
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`);
 })
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+  if (msg.author.bot) return;
+
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote));
   }
 })
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
