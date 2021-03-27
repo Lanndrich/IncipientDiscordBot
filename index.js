1a15fabfde6df56a06ba673b513de12c0057cc33
Lanndrich <lanndrich@gmail.com>
2021-03-27 22:14:25 +0000

    App replies to sad words with 'encouragement.'

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 const client = new Discord.Client();
 
+const sadWords = ["sad", "depressed", "unhappy", "angry", "miserable"];
+
+const encouragements = [
+  "Maybe if you were just better at stuff and things you wouldn't feel that way. :<",
+  "Be well pls. <3",
+  "Meow meow meow."
+];
 
 function getQuote() {
   return fetch("https://zenquotes.io/api/random")
@@ -22,6 +29,11 @@ client.on("message", msg => {
 
   if (msg.content === "$inspire") {
     getQuote().then(quote => msg.channel.send(quote));
+  }
+
+  if (sadWords.some(word => msg.content.includes(word))) {
+    const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)];
+    msg.reply(encouragement);
   }
 })
 
